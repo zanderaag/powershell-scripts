2a721a2327fe6ebfb7249f06e7e9cb27bd7f383f
Adam Goralczyk <48138474+zanderaag@users.noreply.github.com>
2020-09-30 20:15:14 -0400

    Delete Get-SessionGUI.ps1

--- Get-SessionGUI.ps1
+++
@@ -1,6 +0,0 @@
-﻿function Get-SessionGUI (){
-$sesh = Read-Host "Please enter a session name"
-$comp = Read-Host "Please enter a computer name"
-New-PSSession -ComputerName $comp -Name $sesh > null #suppress this cmdlet's output so it's not doubled up
-Get-PSSession #display all sessions
-}
